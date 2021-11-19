9c65efbd1daffb32cb8c62a957458b6733017d80
Terettetererere <t193024@edu.kaiyodai.ac.jp>
2021-11-19 18:20:31 +0900

    2nd commit

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import pandas as pd 
 import plotly.express as px
+from importlib.resources import *
 
 st.title("Streamlit Demo")
 st.subheader("by S.T.")
+
+st.write("1234")
 
 df = px.data.iris()
 
 st.write(df)
 
 st.write(px.scatter(df,x="sepal_length",y="sepal_width",color="species"))
+
+st.write("abc")
