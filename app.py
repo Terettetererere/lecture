e3fbe5ae414353fe4f3579229201cb08851ebc79
teraoka <92132913+Terettetererere@users.noreply.github.com>
2021-11-22 12:43:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd 
 from mypulp import Model
-from importlib.resources import *
+#from importlib.resources import *
 
 st.set_page_config(
     page_title="SaizeApp ",
